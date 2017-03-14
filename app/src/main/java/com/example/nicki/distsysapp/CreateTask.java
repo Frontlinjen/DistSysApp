9e1a37fdd55d601ad33edbd3294446115904b3fb
Nicki <nduris@hotmail.com>
2017-03-14 03:17:40 +0100

    Mainmenu & createTask has gotten basic functions in

--- app/src/main/java/com/example/nicki/distsysapp/CreateTask.java
+++ app/src/main/java/com/example/nicki/distsysapp/CreateTask.java
@@ -2,12 +2,50 @@ package com.example.nicki.distsysapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import com.example.nicki.distsysapp.DatabaseController.TaskDTO;
 
 public class CreateTask extends AppCompatActivity {
+
+    EditText title, description, price, provider, urgency, adress;
+    Button cancel, create;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        title = (EditText) findViewById(R.id.title);
+        description = (EditText) findViewById(R.id.description);
+        price = (EditText) findViewById(R.id.price);
+        provider = (EditText) findViewById(R.id.materialProvider);
+        urgency = (EditText) findViewById(R.id.urgency);
+        adress = (EditText) findViewById(R.id.adress);
+
+        cancel = (Button) findViewById(R.id.ctCancel);
+        create = (Button) findViewById(R.id.CreateTask);
+
+        final java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        final Toast toast = Toast.makeText(getApplicationContext(), "Good Job", Toast.LENGTH_SHORT);
+
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskDTO task  = new TaskDTO("id", "title", "description", 1, 1, true, true, 1, "adress", 10, date, date, "id3");
+                toast.show();
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/nicki/distsysapp/MainMenu.java
+++ app/src/main/java/com/example/nicki/distsysapp/MainMenu.java
@@ -1,15 +1,54 @@
 package com.example.nicki.distsysapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.nicki.distsysapp.R;
 
+import javax.ws.rs.WebApplicationException;
+
 public class MainMenu extends AppCompatActivity {
+
+    Button bC;
+    Button bS;
+    Button bE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        bC= (Button) findViewById(R.id.CreateTask);
+        bS= (Button) findViewById(R.id.ShowTaskList);
+        bE= (Button) findViewById(R.id.Edit_or_delete_task);
+        final Toast toast = Toast.makeText(getApplicationContext(), "Not yet Implemented", Toast.LENGTH_SHORT);
+
+        bC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(),CreateTask.class);
+                    startActivity(i);
+                }
+
+            });
+
+        bS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast.show();
+            }
+        });
+
+        bE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast.show();
+            }
+        });
+
     }
 }
